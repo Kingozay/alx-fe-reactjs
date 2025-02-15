66b44db7b134e669889be28f656d24ca18a1d575
Goodluck Oluwaseun <goodluck98seun@gmail.com>
2025-02-15 05:52:51 +0100

    Added WelcomeMessage component and modified JSX

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,11 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './UserProfile';
 
 function App() {
   return (
     <div>
       <h1>Hello, ALX React Project!</h1>
+      <WelcomeMessage />
       <UserProfile name="John Doe" email="john@example.com" location="New York" />
       <UserProfile name="Jane Smith" email="jane@example.com" location="London" />
     </div>
