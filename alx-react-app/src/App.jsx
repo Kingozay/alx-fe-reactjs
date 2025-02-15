302284c08ab80151ffc8ae56d912739603273072
Goodluck Oluwaseun <goodluck98seun@gmail.com>
2025-02-15 06:02:16 +0100

    Added Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,13 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import UserProfile from './UserProfile';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <h1>Hello, ALX React Project!</h1>
-      <WelcomeMessage />
-      <UserProfile name="John Doe" email="john@example.com" location="New York" />
-      <UserProfile name="Jane Smith" email="jane@example.com" location="London" />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
