39e7f55070bd9635d3f3ebe6003be6857ac98fd0
Goodluck Oluwaseun <goodluck98seun@gmail.com>
2025-03-24 16:20:30 +0100

    Reorganized project structure with all files in my-company directory

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,35 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+﻿import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Services from './Services';
 import Contact from './Contact';
-import Navbar from './Navbar';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Navbar />
-      
       <Routes>
-        <Route path="/" element={
-          <>
-            <MainContent />
-            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-            <UserProfile name="Bob" age="30" bio="Enjoys traveling and cooking" />
-          </>
-        } />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      
-      <Footer />
     </BrowserRouter>
   );
 }
