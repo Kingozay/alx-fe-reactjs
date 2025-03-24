b8a68221ae34a16685c5f4013d13fa410000bb5b
Goodluck Oluwaseun <goodluck98seun@gmail.com>
2025-03-24 15:03:40 +0100

    Convert my-company from submodule to regular directory

---
+++ my-company/alx-react-app-new/src/components/src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>Simple Counter Application</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
